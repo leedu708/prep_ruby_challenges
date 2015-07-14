3539d2cfa3a3d3f25e0fb9e7a205c88b12d3d486
leedu708 <djl265@cornell.edu>
2015-07-14 01:33:14 -0400

    complete overlap method

--- overlap.rb
+++ overlap.rb
@@ -1,18 +1,16 @@
-array1 = [[0,0],[1,4]]
-array2 = [[1,1],[3,2]]
+array1 = [[0,0],[3,3]]
+array2 = [[1,1],[4,5]]
 
 def overlap(arr1, arr2)
 
-  overlap = false
-  for x in arr1[0][0]..arr1[1][0]
-    for y in arr1[0][1]..arr1[1][1]
-      if [x,y] == arr2[0] or [x,y] == arr2[1]
-        overlap = true
-      end
-    end
+  if arr1[1][0] <= arr2[0][0] or arr2[1][0] <= arr1[0][0]
+    return false
+  elsif arr1[1][1] <= arr2[0][1] or arr2[1][1] <= arr1[0][1]
+    return false
   end
 
-  return overlap
+  return true
+
 end
 
 puts overlap(array1,array2)
